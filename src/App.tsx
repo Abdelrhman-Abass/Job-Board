ab619d4584704b5d5770026bd80475ee47020306
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-02-19 06:42:24 +0200

    add tailwind css & react-router-dom
    added tailwind css , react-router-dom and the base folder structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <main className="flex h-screen">
+      {/* Public Routes */ }
+
+      
+      {/* Private Routes */ }
+    </main>
+  )
+}
+
+export default App
